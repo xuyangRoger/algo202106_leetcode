c55260bb6c76c6cec69f5a36b3eee6a6efd75872
xuyangRoger <xuyang_roger@163.com>
2021-06-23 15:07:28 +0800

    merge two sorted lists

---
+++ week01/21merge-two-sorted-lists.cpp
@@ -0,0 +1,69 @@
+/**  递归
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if(l1 == nullptr)
+        {
+            return l2;
+        }
+        else if(l2 == nullptr)
+        {
+            return l1;
+        }
+        else if(l1->val < l2->val)
+        {
+            l1->next = mergeTwoLists(l1->next, l2);
+            return l1;
+        }
+        else
+        {
+            l2->next = mergeTwoLists(l1, l2->next);
+            return l2;
+        }
+    }
+};
+
+
+/** 迭代
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* preHead = new ListNode(0);
+        ListNode* pre = preHead;
+        while(l1 && l2)
+        {
+            if(l1->val < l2->val)
+            {
+                pre->next = l1;
+                l1 = l1->next;
+            }
+            else
+            {
+                pre->next = l2;
+                l2 = l2->next;
+            }
+            pre = pre->next;
+        }
+
+        pre->next = l1 == nullptr ? l2 : l1;
+        return preHead->next;
+    }
+};
